26ac4e48555f8f20494cc5be81ce46d145207442
Huzaifa-Farhan <huzaifa.farhan042@gmail.com>
2024-03-29 23:43:09 -0400

    Test

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+const express =  require('express');
+const app = express();
+
+app.get('/users/test', (req, res) => {
+    res.send({msg: 'Testing'});
+
+});
+
+app.listen(5000, () => consolse.log('Running on Port 5000'));
