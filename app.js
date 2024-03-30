aeb19f26bc925c6c844e4700587707c0e78508fc
Huzaifa-Farhan <huzaifa.farhan042@gmail.com>
2024-03-30 00:04:47 -0400

    Deploying Express changes

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express =  require('express');
 const app = express();
 
 app.get('/users/test', (req, res) => {
-    res.send({msg: 'Testing'});
+    res.send({msg: 'Hello World'});
 
+});
+
+app.get('/users', (req, res) => {
+    res.send({
+        data: [
+            {name: 'huzaifa'}
+        ]
+    });
 });
 
 app.listen(5000, () => consolse.log('Running on Port 5000'));
